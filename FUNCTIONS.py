07682270f98dd87642e7223c08a2bd63f033b929
Kevin Fernando <77948222+kevinfernaando@users.noreply.github.com>
2023-08-24 14:15:12 +0000

    Update FUNCTIONS.py

--- FUNCTIONS.py
+++ FUNCTIONS.py
@@ -6,6 +6,7 @@ import warnings
 import seaborn as sns
 import nltk
 
+nltk.download('words')
 
 warnings.filterwarnings('ignore')
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
